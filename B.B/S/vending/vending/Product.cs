ebf6b4855bfc41e1efd81ece2aeb922bfd61b2ea
Grasa <gulyas.gergely22k@egressy.info>
2026-09-17 12:32:05 +0200

    Creating Properties and Construktor in Product

---
+++ B.B/S/vending/vending/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending
+{
+    internal class Product
+    {
+        private string code;
+        private string name;
+        private int price;
+        private int stock;
+        private ProductCategory category;
+
+        public string Code { get { return code; } }
+        public string Name { get { return name; } }
+        public int Price { get { return price; } }
+        public int Stock { get { return stock; } }
+        public ProductCategory Category { get { return category; } }
+        public bool IsAvailable
+        {
+            get
+            {
+                if (stock >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+             }
+        }
+
+        public Product(string code, string name, int price, int stock, ProductCategory category)
+        {
+            this.code = code;
+            this.name = name;
+            this.price = price;
+            this.stock = stock;
+            this.category = category;
+        }
+
+        public void Sell()
+        {
+            stock--;
+        }
+
+
+
+    }
+}
---
+++ B.B/S/vending/vending/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace vending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+        }
+    }
+}
